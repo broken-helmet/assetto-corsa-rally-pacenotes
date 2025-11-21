7f3e01eab6bc0356e8070edcd234b575904015dd
Koen van Hove <koen@koenvh.nl>
2025-11-21 23:52:27 +0100

    Update script for converting voices

--- pacenote_generation/convert_acr.py
+++ pacenote_generation/convert_acr.py
@@ -2,11 +2,18 @@ import os
 import shutil
 
 
-base = ''
+language = "Spanish"
+
+base = f'B:/Koen/Documents/Assetto Corsa Rally/Original pacenotes/{language}/Non Filtered'
 for file in os.listdir(base):
     if file.endswith("_Any_Any_1.wav"):
         clean_file = file.replace("PN_", "").replace("_Any_Any_1", "")
-        shutil.copy2(base + "/" + file, "../voices/English/" + clean_file)
+        clean_file = clean_file.replace("Uphill", "UpHill")
+        shutil.copy2(f"{base}/{file}", f"../voices/{language}/{clean_file}")
         print(clean_file)
 
 print("Done.")
+print(set(os.listdir("../voices/Dutch")).difference(set(os.listdir(f"../voices/{language}"))))
+for file in set(os.listdir(f"../voices/{language}")).difference(set(os.listdir("../voices/Dutch"))):
+    os.remove(f"../voices/{language}/{file}")
+    print(file)
